455053ad7ac1f3f67e48bd7c0a762f87d9349fd8
sylvain <sylvain@sylvain-linux>
2017-07-06 00:13:54 +0200

    test

--- opencv_roi2.py
+++ opencv_roi2.py
@@ -10,6 +10,7 @@ from PIL import Image
 from PIL import ImageTk
 import tkinter.filedialog
 import cv2
+import pytesseract
 
 # Choose file
 path = tkinter.filedialog.askopenfilename()
@@ -23,7 +24,13 @@ r = cv2.selectROI("Image", im, fromCenter = False)
 # Crop image
 imCrop = im[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
 
+# Convert image to PIL
+image = cv2.cvtColor(imCrop, cv2.COLOR_BGR2RGB)
+image = Image.fromarray(image)
+
 # Display cropped image
 cv2.imshow("Image", imCrop)
 cv2.imshow("Origin",im)
+txt = pytesseract.image_to_string(image)
+print(txt)
 cv2.waitKey(0)
